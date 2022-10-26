52ab47e5f4135670b5df582bb05b9b6abd79bdd6
harry-88 <harisbro88@gmail.com>
2022-10-26 10:03:09 +0500

    some issue while import 1 link report

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 const getRecord = () => {
 
-    const file = XLSX.readFile('D://parse-xlsx-in-node/empty.xls');
+    const file = XLSX.readFile('D://parse-xlsx-in-node/21.xls');
     let data = []
     const sheets = file.SheetNames
     for (let i = 0; i < sheets.length; i++) {
@@ -17,21 +17,28 @@ const getRecord = () => {
         data = temp;
     }
     let record = [];
-    let flag = 0;
+    let flag = true;
+    let index = 0;
+
     for (let i = 0; i < data.length; i++) {
         let obj = {};
+        index = i;
         if (Object.keys(data[i]).length === 17) {
-            let key = Object.values(data[i]);
-            let value = Object.values(data[i + 1]);
-            if (key.length === value.length) {
-                obj[Object.values(data[i - 1])[0]] = Object.values(data[i - 1])[1]
-                for (let x = 0; x < key.length; x++) {
-                    obj[key[x]] = value[x]
+            let key = Object.values(data[index]);
+            let value = Object.values(data[index + 1]);
+            while (Object.values(data[index + 1]).length === key.length){
+                if (key.length === value.length) {
+                    obj[Object.values(data[index - 1])[0]] = Object.values(data[index - 1])[1]
+                    for (let x = 0; x < key.length; x++) {
+                        obj[key[x]] = value[x]
+                    }
+                    record.push(obj)
+                    index++;
                 }
-                record.push(obj)
-                i++;
             }
         }
+
+        i = index;
     }
     console.log("record is ", record)
 }
